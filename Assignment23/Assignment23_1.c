d9d2df755d62914e0901597b0ef00b5acd6dba5e
pratiknamdas_ <pratiknamdas1024@gmail.com>
2025-06-12 04:46:05 +0530

    12th june

--- Assignment23/Assignment23_1.c
+++ Assignment23/Assignment23_1.c
@@ -1,31 +1,19 @@
 #include<stdio.h>
 
-void DisplayASCII(char ch)
+void DisplayASCII()
 {
-    
+    int i = 0;
 
-    if((ch >= 0) && (ch <= 255))
-    {
-        printf("%c\n",ch);
-    }
-    else
-    {
-        printf("Invalid input\n");
-    }
+   for(i = 0; i <= 255; i++)
+   {
+        printf("ASCII value of %c is %d \n",i,i);
+   }
 }
 
 
 int main()
 {
-    char cValue = '\0';
-    
 
-    printf("Enter The Character\n");
-    scanf("%c",&cValue);
-
-    DisplayASCII(cValue);
-
-
-    
+    DisplayASCII();    
     return 0;
 }
